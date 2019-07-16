e4d8063a18af349402384eb06f10f064d2a09de6
Genevieve Kankam Mensah <akuanhanha@gmail.com>
2019-07-16 15:51:03 +0000

    i built my first webpage today, lol

---
+++ hut/my_app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return 'Hello world'
+
+
+@app.route('/whereami')
+def whereami():
+    return 'whereami'
+ 
+ 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
